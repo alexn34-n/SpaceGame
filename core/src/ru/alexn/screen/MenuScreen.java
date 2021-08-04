df6fa35266fa2aef8b12c6c5d4248ada6bd52fe3
alexn34-n <alexn3469@mail.ru>
2021-08-04 10:39:10 +0300

    Выполненная домашняя работа  к 2 уроку.

--- core/src/ru/alexn/screen/MenuScreen.java
+++ core/src/ru/alexn/screen/MenuScreen.java
@@ -8,16 +8,24 @@ import com.badlogic.gdx.math.Vector2;
 import ru.alexn.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
-   private Texture img;
+
+    private static  final float V_LEN = 0.65f;
+
+    private Texture img;
+    private Vector2 touch;
     private Vector2 pos;
     private Vector2 v;
+    private Vector2 tmp;
+
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        touch = new Vector2();
         pos = new Vector2();
-        v = new Vector2(1, 1);
+        v = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
@@ -26,7 +34,13 @@ public class MenuScreen extends BaseScreen {
         batch.begin();
         batch.draw(img,pos.x,pos.y);
         batch.end();
-        pos.add(v);
+        tmp.set(touch);
+        if(tmp.sub(pos).len()>V_LEN){
+            pos.add(v);
+        }else {
+            pos.set(touch);
+        }
+
     }
 
     @Override
@@ -37,13 +51,14 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight()-screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
+        touch.set(screenX, Gdx.graphics.getHeight()-screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
+        return false;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         pos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDragged(screenX, screenY, pointer);
+        return false;
     }
 }
